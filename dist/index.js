b11b2fa4752fc215c7502b6870740576afc33de4
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-06-19 01:55:12 +0500

    Unused Parameters option

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
-const score = 50;
+let score = 0;
+let age;
+let name = 'Uzair';
+console.log('name: ', name);
+module.exports = {};
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-const score: number = 50;
+export = {}
+
+let score = 0;
+let age:number;
+
+let name = 'Uzair';
+
+let numbers:number[] = [1,2,3,4,5];
+
+let Mixed = [1, "2"]
+
+function calculateTax(amount:number):number {
+    // return amount * 0.08;
+}
