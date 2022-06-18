b4fc1c1f8a91333e8cd9d088fd20fd285b23e192
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-06-19 01:58:33 +0500

    No implicit returns

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 export = {}
 
-let score = 0;
-let age:number;
+// let score = 0;
+// let age:number;
 
-let name = 'Uzair';
+// let name = 'Uzair';
 
-let numbers:number[] = [1,2,3,4,5];
+// let numbers:number[] = [1,2,3,4,5];
 
-let Mixed = [1, "2"]
+// let Mixed = [1, "2"]
 
 function calculateTax(amount:number):number {
-    let x;
-    return amount * 0.08;
+    if(amount > 0) {
+        return amount * 0.1;
+    }
+    return amount * 0.2;
 }
